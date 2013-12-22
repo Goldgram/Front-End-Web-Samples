d3f04e47401d6fef777c4907f8cf08edf9cd1304
Goldgram <willbrennandesign@gmail.com>
2013-12-22 13:45:03 +0000

    building mobile custom zoom, more work is needed in testing touch screen and appropriate responses.

--- ZG-Interview-Project/resources/js/main.js
+++ ZG-Interview-Project/resources/js/main.js
@@ -14,8 +14,8 @@ $(document).ready(function() {
 			$("#"+thisID).val("");
 		}
 	});
-  /* custom zoom js */
-	$("#popUpZoomImage").mousemove(function(event) {
+  /* custom zoom js desktop*/
+	$("#popUpZoomImageDesktop").mousemove(function(event) {
     var xPos,yPos;
     // Firefox bug fix for offset x and y
     if(event.offsetX===undefined) {
@@ -29,8 +29,45 @@ $(document).ready(function() {
     var yPercentage = parseInt(((yPos*100)/$(this).height()),10);
     $(this).css({"background-position":xPercentage+"% "+yPercentage+"%"});
   });
+  /* mobile zoom, TODO: currently working on touch screen functionality */
+  // $(".popUpZoomArrow").click(function (){
+  //   var xAdd,yAdd,xPercentage,yPercentage;
+  //   switch($(this).attr("id")){
+  //     case "popUpZoomRight":
+  //     xAdd = 20;
+  //     yAdd = 0;
+  //     break;
+  //     case "popUpZoomDown":
+  //     xAdd = 0;
+  //     yAdd = 20;
+  //     break;
+  //     case "popUpZoomLeft":
+  //     xAdd = -20;
+  //     yAdd = 0;
+  //     break;
+  //     case "popUpZoomUp":
+  //     xAdd = 0;
+  //     yAdd = -20;
+  //   }
+  //   var backgroundPos = $("#popUpZoomImageTouchScreen").css("background-position").split(" ");
+  //   if (backgroundPos[0].indexOf("px") !== -1) {
+  //     xPercentage = parseInt(((backgroundPos[0].slice(0,-2)*100)/$("#popUpZoomImageTouchScreen").width()),10)+xAdd;
+  //   } else {
+  //     xPercentage = parseInt(backgroundPos[0].slice(0,-1),10)+xAdd;
+  //   }
+  //   if (backgroundPos[1].indexOf("px") !== -1) {
+  //     yPercentage = parseInt(((backgroundPos[1].slice(0,-2)*100)/$("#popUpZoomImageTouchScreen").height()),10)+yAdd;
+  //   } else {
+  //     yPercentage = parseInt(backgroundPos[1].slice(0,-1),10)+yAdd;
+  //   }
+  //   $("#popUpZoomImageTouchScreen").css({"background-position":xPercentage+"% "+yPercentage+"%"});
+  //   (xPercentage<=0) ? $("#popUpZoomLeft").hide() : $("#popUpZoomLeft").show();
+  //   (xPercentage>=100) ? $("#popUpZoomRight").hide() : $("#popUpZoomRight").show();
+  //   (yPercentage<=0) ? $("#popUpZoomUp").hide() : $("#popUpZoomUp").show();
+  //   (yPercentage>=100) ? $("#popUpZoomDown").hide() : $("#popUpZoomDown").show();
+  // });
 	// show and hide custom zoom
-  $(".showHideideCustomZoom ").click(function (){
+  $(".showHideideCustomZoom").click(function (){
 		$("#popUpDivOpaque,#popUpZoomImage,#zoomCancel").toggle();
   });
   // show large image title
@@ -43,8 +80,7 @@ $(document).ready(function() {
   $(".hoverPrice").mouseenter(function() {
     $(this).children(":first").addClass("tileBackgroundImage");
     $(this).children(":first").next().show();
-  })
-  .mouseleave(function() {
+  }).mouseleave(function() {
     $(this).children(":first").removeClass("tileBackgroundImage");
     $(this).children(":first").next().hide();
   });
